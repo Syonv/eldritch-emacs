3a765fe69a5cfa46e082fed82773344d63bf5451
Syonv <glenn.lang04@gmail.com>
2024-10-04 16:39:59 +0200

    Initial commit

---
+++ eldritch-theme.el
@@ -0,0 +1,35 @@
+;;; eldritch-theme.el --- Eldritch color theme for Emacs.
+
+;; Author: Glenn S. Lang
+;; Version: 0.1
+
+(deftheme eldritch
+  "Eldritch color theme for Emacs")
+
+(let ((eldritch-background     "#212337")
+      (eldritch-currentline    "#323449")
+      (eldritch-foreground     "#ebfafa")
+      (eldritch-comment        "#7081d0")
+      (eldritch-cyan           "#04d1f9")
+      (eldritch-green          "#37f499")
+      (eldritch-orange         "#f7c67f")
+      (eldritch-pink           "#f265b5")
+      (eldritch-purple         "#a48cf2")
+      (eldritch-red            "#f16c75")
+      (eldritch-yellow         "#f1fc79")
+      (eldritch-g-dark         "#171928")
+      (eldritch-bg-highlight   "#292e42")
+      (eldritch-terminal-black "#414868")
+      (eldritch-fg-dark        "#abb4da")
+      (eldritch-fg-gutter      "#3b4261")
+      (eldritch-dark3          "#6473B7")
+      (eldritch-comment        "#7081d0")
+      (eldritch-dark5          "#5866a2")
+      (eldritch-visual         "#76639e")
+      (eldritch-dark-cyan      "#10a1bd")
+      (eldritch-magenta2       "#bf4f8e")
+      (eldritch-magenta3       "#722f55")
+      (eldritch-dark-yellow    "#c0c95f")
+      (eldritch-dark-green     "#33c57f")
+      (eldritch-bright-red     "#f0313e")
+      ))
